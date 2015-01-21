42b1edaaf9ac8722c2b3b254ca685eb0c1acde3b
kevinttan <kevin.tolentino.tan@live.com>
2015-01-21 16:22:35 +0800

    added comment using github online

--- ForTesting/ForTesting/Program.cs
+++ ForTesting/ForTesting/Program.cs
@@ -14,6 +14,7 @@ namespace ForTesting
         [STAThread]
         static void Main()
         {
+            //ADDING THE COMMENT AGAIN!!
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
